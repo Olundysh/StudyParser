8cd5baee56e9ed458f9bd72233aa981be67e118d
olundysh <olgavecholga@gmail.com>
2023-02-10 16:27:25 +0300

    A version which can not see tab.

---
+++ SentencesParserTask.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextAnalysis
+{
+    static class SentencesParserTask
+    {
+       public static List<string> ParseWords(string sentence)
+        {
+            var word = new StringBuilder();
+            var wordList = new List<string>();
+
+            for (var i = 0; i < sentence.Length; i++)
+            {
+                if (char.IsLetter(sentence[i]) || sentence[i] == '\'')
+                {
+                    word.Append(sentence[i]);
+                    if (i == sentence.Length - 1)
+                    {
+                        wordList.Add(word.ToString().ToLower());
+                    }
+                    
+                }
+                else
+                {
+                    {
+                        if (!word.Equals(new StringBuilder(0)))
+                            wordList.Add(word.ToString().ToLower());
+                    }
+
+                    word.Remove(0, word.Length);
+                }
+            }
+            
+            return wordList;
+        }
+
+      public  static List<List<string>> ParseSentences(string text)
+        {
+
+            var separators = new[] { '.', '!', '?', ';', ':', '(', ')', '\t', '\n', '\r' };
+            var sentences = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            var sentencesList = new List<List<string>>();
+
+            foreach (var t in sentences)
+            {
+                sentencesList.Add(ParseWords(t));
+            }
+
+            return sentencesList;
+        }
+    }
+}
