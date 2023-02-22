c011fbcb0793d52a070d107a9f7141ab6b12cf35
olundysh <olgavecholga@gmail.com>
2023-02-22 16:06:53 +0300

    TextGenerator Task finished; tests passed.

--- TextGeneratorTask.cs
+++ TextGeneratorTask.cs
@@ -1,15 +1,27 @@
-﻿using System.Collections.Generic;
+﻿
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TextAnalysis
 {
     static class TextGeneratorTask
     {
-        public static string ContinuePhrase(
-            Dictionary<string, string> nextWords,
-            string phraseBeginning,
-            int wordsCount)
+        public static string ContinuePhrase(Dictionary<string, string> nextWords, string phraseBeginning, int wordsCount)
         {
-            return phraseBeginning;
+            var splittedPhrase = phraseBeginning.Split(' ').ToList();
+            for (var i = 0; i < wordsCount; i++)
+            {
+                if (splittedPhrase.Count >=2 && nextWords.TryGetValue($"{splittedPhrase[splittedPhrase.Count - 2]} {splittedPhrase[splittedPhrase.Count - 1]}", out var value2))
+                {
+                    splittedPhrase.Add(value2);
+                    continue;
+                }
+                if (splittedPhrase.Count >=1 && nextWords.TryGetValue(splittedPhrase[splittedPhrase.Count - 1], out var value1))
+                {
+                    splittedPhrase.Add(value1);
+                }
+            }
+            return string.Join(" ", splittedPhrase);
         }
     }
 }
